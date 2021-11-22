f8715f500b2243c54662605911877283eaa77f78
shalinikaMalshani <it20233976@my.sliit.lk>
2021-11-22 20:37:47 +0530

    add secret baker project strucher

---
+++ src/main/java/com/example/secret_baker/SecretBakerApplication.java
@@ -0,0 +1,13 @@
+package com.example.secret_baker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecretBakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecretBakerApplication.class, args);
+	}
+
+}
